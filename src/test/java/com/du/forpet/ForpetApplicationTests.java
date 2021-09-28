f25f699c1374af6f28b864b7bbffdfe1510b6ff8
daulkim <kimdu0825@gmail.com>
2021-09-28 22:13:58 +0900

    Initial commit

---
+++ src/test/java/com/du/forpet/ForpetApplicationTests.java
@@ -0,0 +1,13 @@
+package com.du.forpet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ForpetApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
