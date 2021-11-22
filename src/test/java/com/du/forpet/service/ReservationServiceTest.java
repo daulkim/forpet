647e0b0d7b9e80169f4d8c37b1b6c138e2fc7232
daulkim <kimdu0825@gmail.com>
2021-11-22 23:43:08 +0900

    ReservationServiceTest

--- src/test/java/com/du/forpet/service/ReservationServiceTest.java
+++ src/test/java/com/du/forpet/service/ReservationServiceTest.java
@@ -4,15 +4,13 @@ import com.du.forpet.domain.ReservationStatus;
 import com.du.forpet.domain.dto.*;
 import com.du.forpet.domain.entity.Helper;
 import com.du.forpet.repository.HelperRepository;
-import com.du.forpet.repository.ReservationRepository;
 import com.du.forpet.repository.ReservationRepositorySupport;
-import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.data.jpa.repository.support.QuerydslRepositorySupport;
 
-import java.time.LocalDateTime;
+import java.time.LocalDate;
+import java.time.LocalTime;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -21,9 +19,6 @@ public class ReservationServiceTest {
 
     @Autowired
     private ReservationService reservationService;
-
-    @Autowired
-    private ReservationRepository reservationRepository;
 
     @Autowired
     private HelperRepository helperRepository;
@@ -38,61 +33,95 @@ public class ReservationServiceTest {
 
     @Test
     public void testQuerydsl() {
-        long count = reservationRepositorySupport.countByHelperAndDateTime(5L);
+        long count = reservationRepositorySupport.countByHelperAndDateTime(5L, LocalDate.now(), LocalTime.now(), LocalTime.now().plusHours(1));
         assertThat(count).isEqualTo(1L);
     }
 
     @Test
     public void saveTest() {
-        LocalDateTime reservationTime = LocalDateTime.of(2021,11,20,13,00);
+        LocalDate date = LocalDate.of(2021,11,23);
+        LocalTime sTime = LocalTime.of(12,00);
 
         Helper helper = helperRepository.findById(5L).get();
 
         ReservationSaveRequestDto requestDto = ReservationSaveRequestDto
                                                     .builder()
                                                     .serviceType("Bath")
-                                                    .startTime(reservationTime)
-                                                    .endTime(reservationTime.plusHours(1))
+                                                    .reserveDate(date)
+                                                    .startTime(sTime)
+                                                    .endTime(sTime.plusHours(1))
                                                     .helper(helper)
                                                     .build();
+
         Long reservationId = reservationService.save(requestDto);
         ReservationStatus status = reservationService.findById(reservationId).getStatus();
 
-        assertThat(status).isEqualTo(ReservationStatus.P);
+        assertThat(status).isEqualTo(ReservationStatus.PRE);
+
+    }
+
+    @Test
+    public void saveFailTest() {
+        LocalDate date = LocalDate.of(2021,11,23);
+        LocalTime sTime = LocalTime.of(12,00);
+
+        Helper helper = helperRepository.findById(5L).get();
+
+        ReservationSaveRequestDto requestDto = ReservationSaveRequestDto
+                                                        .builder()
+                                                        .serviceType("Bath")
+                                                        .reserveDate(date)
+                                                        .startTime(sTime)
+                                                        .endTime(sTime.plusHours(1))
+                                                        .helper(helper)
+                                                        .build();
+
+        Long reservationId = reservationService.save(requestDto);
+        ReservationStatus status = reservationService.findById(reservationId).getStatus();
+
+        assertThat(status).isEqualTo(ReservationStatus.PRE);
 
     }
 
     @Test
     public void cancelSuccTest() {
-        LocalDateTime reservationTime = LocalDateTime.of(2021,11,15,13,00);
+        LocalDate date = LocalDate.of(2021,11,20);
+        LocalTime sTime = LocalTime.of(12,00);
 
-        Helper helper = helperRepository.findById(1L).get();
+        Helper helper = helperRepository.findById(5L).get();
         ReservationSaveRequestDto requestDto = ReservationSaveRequestDto
-                                                    .builder()
-                                                    .serviceType("Bath")
-                                                    .startTime(reservationTime)
-                                                    .endTime(reservationTime.plusHours(1))
-                                                    .helper(helper)
-                                                    .build();
+                                                        .builder()
+                                                        .serviceType("Bath")
+                                                        .reserveDate(date)
+                                                        .startTime(sTime)
+                                                        .endTime(sTime.plusHours(1))
+                                                        .helper(helper)
+                                                        .build();
+
         Long reservationId = reservationService.save(requestDto);
 
         reservationService.cancel(reservationId);
 
         ReservationStatus status = reservationService.findById(reservationId).getStatus();
-        assertThat(status).isEqualTo(ReservationStatus.C);
+        assertThat(status).isEqualTo(ReservationStatus.CANCEL);
 
     }
 
     @Test
     public void cancelFailTest() {
-        LocalDateTime reservationTime = LocalDateTime.now().plusHours(3);
+        LocalDate date = LocalDate.of(2021,11,20);
+        LocalTime sTime = LocalTime.of(12,00);
 
+        Helper helper = helperRepository.findById(5L).get();
         ReservationSaveRequestDto requestDto = ReservationSaveRequestDto
-                                                    .builder()
-                                                    .serviceType("Bath")
-                                                    .startTime(reservationTime)
-                                                    .endTime(reservationTime.plusHours(3))
-                                                    .build();
+                                                        .builder()
+                                                        .serviceType("Bath")
+                                                        .reserveDate(date)
+                                                        .startTime(sTime)
+                                                        .endTime(sTime.plusHours(1))
+                                                        .helper(helper)
+                                                        .build();
+
         Long reservationId = reservationService.save(requestDto);
 
         reservationService.cancel(reservationId);
@@ -101,43 +130,48 @@ public class ReservationServiceTest {
 
     @Test
     public void updateSuccTest() {
-        LocalDateTime reservationTime = LocalDateTime.now().plusDays(3);
 
+        LocalDate date = LocalDate.of(2021,11,20);
+        LocalTime sTime = LocalTime.of(12,00);
+
+        Helper helper = helperRepository.findById(5L).get();
         ReservationSaveRequestDto requestDto = ReservationSaveRequestDto
-                                                    .builder()
-                                                    .serviceType("Bath")
-                                                    .startTime(reservationTime)
-                                                    .endTime(reservationTime.plusHours(3))
-                                                    .build();
+                                                        .builder()
+                                                        .serviceType("Bath")
+                                                        .reserveDate(date)
+                                                        .startTime(sTime)
+                                                        .endTime(sTime.plusHours(1))
+                                                        .helper(helper)
+                                                        .build();
 
         Long reservationId = reservationService.save(requestDto);
 
         ReservationUpdateRequestDto updateDto = ReservationUpdateRequestDto
-                                                    .builder()
-                                                    .startTime(LocalDateTime.now())
-                                                    .endTime(LocalDateTime.now().plusHours(3))
-                                                    .build();
+                                                        .builder()
+                                                        .startTime(sTime)
+                                                        .endTime(sTime.plusHours(3))
+                                                        .build();
 
         reservationService.update(reservationId, updateDto);
 
         ReservationResponseDto reservationResponseDto = reservationService.findById(reservationId);
 
-        assertThat(reservationResponseDto.getStartTime()).isBefore(LocalDateTime.now());
-        assertThat(reservationResponseDto.getEndTime()).isBefore(LocalDateTime.now().plusHours(3));
-        assertThat(reservationResponseDto.getStatus()).isEqualTo(ReservationStatus.P);
+        assertThat(reservationResponseDto.getStartTime()).isBefore(LocalTime.now());
+        assertThat(reservationResponseDto.getEndTime()).isBefore(LocalTime.now().plusHours(3));
+        assertThat(reservationResponseDto.getStatus()).isEqualTo(ReservationStatus.PRE);
 
     }
 
     @Test
     public void updateFailTest() {
-        LocalDateTime reservationTime = LocalDateTime.now().plusDays(3);
+        LocalTime sTime = LocalTime.of(12,00);
 
         ReservationSaveRequestDto requestDto = ReservationSaveRequestDto
-                .builder()
-                .serviceType("Bath")
-                .startTime(reservationTime)
-                .endTime(reservationTime.plusHours(3))
-                .build();
+                                                        .builder()
+                                                        .serviceType("Bath")
+                                                        .startTime(sTime)
+                                                        .endTime(sTime.plusHours(3))
+                                                        .build();
 
         Long reservationId = reservationService.save(requestDto);
 
@@ -146,8 +180,8 @@ public class ReservationServiceTest {
 
         ReservationUpdateRequestDto updateDto = ReservationUpdateRequestDto
                                                     .builder()
-                                                    .startTime(LocalDateTime.now())
-                                                    .endTime(LocalDateTime.now().plusHours(3))
+                                                    .startTime(LocalTime.now())
+                                                    .endTime(LocalTime.now().plusHours(3))
                                                     .build();
 
         reservationService.update(reservationId, updateDto);
