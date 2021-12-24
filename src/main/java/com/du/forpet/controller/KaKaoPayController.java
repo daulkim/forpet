6e05d5ef0f92b86a1db5d018ba4e7cab86aca57b
daulkim <kimdu0825@gmail.com>
2021-12-24 23:39:26 +0900

    Kakao pay API 결제페이지 Redirect

--- src/main/java/com/du/forpet/controller/KaKaoPayController.java
+++ src/main/java/com/du/forpet/controller/KaKaoPayController.java
@@ -6,15 +6,17 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
 @RequiredArgsConstructor
 @RestController
-public class KaKaoPayContoller {
+public class KaKaoPayController {
 
     private final KakaoPayService kakaoPayService;
 
     @GetMapping("/pay/test")
-    public KakaoReadyResponseDto testReady() {
-
-        return kakaoPayService.ready();
+    public void testReady(HttpServletResponse response) throws IOException {
+        response.sendRedirect(kakaoPayService.ready().getNext_redirect_pc_url());
     }
 }
