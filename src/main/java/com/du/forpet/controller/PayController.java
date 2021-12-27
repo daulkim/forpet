5e15de0aa11bc65f21a3549032473e2b2eec7d36
daulkim <kimdu0825@gmail.com>
2021-12-27 23:15:32 +0900

    Mapping 수정

--- src/main/java/com/du/forpet/controller/PayController.java
+++ src/main/java/com/du/forpet/controller/PayController.java
@@ -1,31 +1,29 @@
 package com.du.forpet.controller;
 
-import com.du.forpet.domain.PayStatus;
 import com.du.forpet.domain.dto.PayResponseDto;
 import com.du.forpet.domain.dto.PaySaveRequestDto;
 import com.du.forpet.service.PayService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 
-import javax.websocket.server.PathParam;
-
+@RequestMapping("/pay")
 @RequiredArgsConstructor
 @RestController
 public class PayController {
 
     private final PayService payService;
 
-    @PostMapping("/pay")
+    @PostMapping()
     public Long pay(@RequestBody PaySaveRequestDto payDto) {
         return payService.save(payDto);
     }
 
-    @GetMapping("/pay/{id}")
+    @GetMapping("/{id}")
     public PayResponseDto findById(@PathVariable Long id) {
         return payService.findById(id);
     }
 
-    @PatchMapping("/pay/{id}")
+    @PatchMapping("/{id}")
     public Long cancel(@PathVariable Long id) {
         return payService.cancel(id);
     }
