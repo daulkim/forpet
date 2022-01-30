cdb7e9820e85ce3bb77106ae697bc34e1fde762b
daulkim <kimdu0825@gmail.com>
2022-01-30 22:11:32 +0900

    예약 수정 화면

--- src/main/java/com/du/forpet/controller/ReservationController.java
+++ src/main/java/com/du/forpet/controller/ReservationController.java
@@ -1,14 +1,23 @@
 package com.du.forpet.controller;
 
 import com.du.forpet.domain.ServiceType;
+import com.du.forpet.domain.dto.ReservationResponseDto;
+import com.du.forpet.service.ReservationService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import javax.websocket.server.PathParam;
+
+@RequiredArgsConstructor
 @RequestMapping("/reservations")
 @Controller
 public class ReservationController {
+
+    private final ReservationService reservationService;
 
     @GetMapping("/reserve")
     public String reserve(Model model) {
@@ -16,8 +25,10 @@ public class ReservationController {
         return "reservation-request";
     }
 
-    @GetMapping("/update")
-    public String update() {
+    @GetMapping("/update/{id}")
+    public String update(@PathVariable Long id, Model model) {
+        ReservationResponseDto dto = reservationService.findById(id);
+        model.addAttribute("reservation", dto);
         return "reservation-update";
     }
 }
