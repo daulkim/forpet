6f5c9a18fd3f8825152c8aa4598eb393722a143c
daulkim <kimdu0825@gmail.com>
2022-05-07 22:16:23 +0900

    불필요 import 삭제

--- src/main/java/com/du/forpet/security/SecurityConfig.java
+++ src/main/java/com/du/forpet/security/SecurityConfig.java
@@ -4,8 +4,6 @@ import com.du.forpet.service.CustomUserDetailsService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.authentication.AuthenticationProvider;
-import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
@@ -13,10 +11,7 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
-
-import java.security.AuthProvider;
 
 @RequiredArgsConstructor
 @Configuration
