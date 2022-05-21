e31334062242a283ee7823221f55070105a2445c
daulkim <kimdu0825@gmail.com>
2022-05-22 03:27:20 +0900

    ServiceFee 관련 삭제(ServiceType Entity로 변경)

--- src/main/java/com/du/forpet/repository/ServiceFeeRepository.java
+++
@@ -1,7 +0,0 @@
-package com.du.forpet.repository;
-
-import com.du.forpet.domain.entity.ServiceFee;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ServiceFeeRepository extends JpaRepository<ServiceFee, Long> {
-}
