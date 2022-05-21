ef7510fcc3c76aec9f0e47851254d8ef0e224d03
daulkim <kimdu0825@gmail.com>
2022-05-22 03:22:19 +0900

    MemberApiController 수정

--- src/main/java/com/du/forpet/controller/api/MemberApiController.java
+++ src/main/java/com/du/forpet/controller/api/MemberApiController.java
@@ -1,32 +1,20 @@
 package com.du.forpet.controller.api;
 
-import com.du.forpet.domain.dto.HelperUpdateRequestDto;
 import com.du.forpet.domain.dto.MemberSaveRequestDto;
-import com.du.forpet.domain.dto.MemberUpdateRequestDto;
 import com.du.forpet.service.MemberService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
 
-@RequestMapping("/members")
 @RequiredArgsConstructor
 @RestController
 public class MemberApiController {
 
     private final MemberService memberService;
 
-    @PostMapping("/join")
-    public Long join(@RequestBody MemberSaveRequestDto requestDto) throws Exception {
-        return memberService.save(requestDto);
+    @PostMapping("/api/members")
+    public Long join(@RequestBody MemberSaveRequestDto memberSaveRequestDto) {
+        return memberService.save(memberSaveRequestDto);
     }
-
-    @PutMapping("/{id}")
-    public Long update(@PathVariable Long id, @RequestBody MemberUpdateRequestDto requestDto) {
-        return memberService.update(id, requestDto);
-    }
-
-    @PatchMapping("/{id}/withdrawal")
-    public void withdraw(@PathVariable Long id) {
-        memberService.withdraw(id);
-    }
-
 }
